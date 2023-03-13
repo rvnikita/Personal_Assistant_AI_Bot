9bdbacf03d9a56fe4edc2219eae6cc916cfd0859
Nikita Rvachev <nikita@rvachev.org>
2023-03-13 12:06:42 -0400

    admin_log.py

---
+++ code/admin_log.py
@@ -0,0 +1,14 @@
+import os
+import configparser
+import requests
+
+
+config = configparser.ConfigParser()
+config_path = os.path.dirname(__file__) + '/../config/' #we need this trick to get path to config folder
+config.read(config_path + 'settings.ini')
+
+def admin_log(text):
+    URL = f"https://api.telegram.org/bot{config['BOT']['KEY']}/sendMessage?chat_id={config['BOT']['ADMIN_ID']}&text={text}"
+
+    r = requests.get(url = URL)
+    data = r.json()
