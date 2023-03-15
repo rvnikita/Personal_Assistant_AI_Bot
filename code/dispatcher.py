3eb68c1a533080045569efdc85fadc2c1a559431
Nikita Rvachev <nikita@rvachev.org>
2023-03-15 15:27:51 -0400

    rewrite regexp

--- code/dispatcher.py
+++ code/dispatcher.py
@@ -218,7 +218,7 @@ async def tg_dispatcher(update, context):
             admin_log(f"Message from {update.message.chat.first_name} {update.message.chat.last_name} @{update.message.chat.username} ({update.message.chat.id}): {update.message.text}")
 
             command = None
-            match = re.match(r"^\/(\w+)([\s\S]*)$", update.message.text, re.DOTALL)
+            match = re.match(r"^\/(\w+)\s*([\s\S]*)$", update.message.text, re.DOTALL)
             if match:
                 command = match.group(1)
                 command_args = match.group(2)
